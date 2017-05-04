e4c3d31fc5afa14050af1f7c7df18d41f8bac3a7
Robert Max Rees <robertmaxrees@gmail.com>
2017-05-04 12:21:26 -0700

    getting a basic server running

---
+++ server.js
@@ -0,0 +1,20 @@
+const restify = require('restify'),
+	server = restify.createServer(),
+	http = require('http'),
+	basicResponse = function(req, res, next) {
+		reply.setHeader('content-type', 'text/html');
+		reply.write('Foo');
+
+		reply.end();
+		next();
+	}
+
+http.globalAgent.maxSockets = Infinity;
+
+server.use(restify.CORS());
+server.use(restify.queryParser());
+server.use(restify.gzipResponse());
+server.get('/', basicResponse);
+server.listen(port, function() {
+	console.log('%s listening at %s', server.name, server.url);
+});
