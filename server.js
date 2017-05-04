b8acf90847bddbec8468730fbf7cc3cbd47f59da
Robert Max Rees <robertmaxrees@gmail.com>
2017-05-04 13:03:38 -0700

    forgot about this part.

--- server.js
+++ server.js
@@ -15,6 +15,6 @@ server.use(restify.CORS());
 server.use(restify.queryParser());
 server.use(restify.gzipResponse());
 server.get('/', basicResponse);
-server.listen(80, function() {
+server.listen(process.env.PORT || 4000, function() {
 	console.log('%s listening at %s', server.name, server.url);
 });
